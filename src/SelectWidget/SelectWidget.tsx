8709c01a04e81c287d9f32ae9e8f8e127bd94617
Shady Rafehi <shadyrafehi@gmail.com>
2020-02-08 16:21:32 +1100

    SelectWidget - Simplify using TextField with "select" and "SelectProps", add support for "ui:muiOptions"

--- src/SelectWidget/SelectWidget.tsx
+++ src/SelectWidget/SelectWidget.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 
-import FormControl from '@material-ui/core/FormControl';
 import MenuItem from '@material-ui/core/MenuItem';
-import Select from '@material-ui/core/Select';
-import InputLabel from '@material-ui/core/InputLabel';
 
-// import { WidgetProps } from 'react-jsonschema-form';
 import { asNumber, guessType } from 'react-jsonschema-form/lib/utils';
+import { getMuiOptions } from '../utils';
+import { TextField } from '@material-ui/core';
 
 const nums = new Set(['number', 'integer']);
 
@@ -71,35 +69,32 @@ const SelectWidget = ({
     onFocus(id, processValue(schema, value));
 
   return (
-    <FormControl
+    <TextField
       fullWidth={true}
-      //error={!!rawErrors}
+      select
+      label={label || schema.title}
+      SelectProps={{
+        multiple: typeof multiple === 'undefined' ? false : multiple,
+      }}
+      value={typeof value === 'undefined' ? emptyValue : value}
       required={required}
+      disabled={disabled || readonly}
+      autoFocus={autofocus}
+      onChange={_onChange}
+      onBlur={_onBlur}
+      onFocus={_onFocus}
+      {...getMuiOptions(options)}
     >
-      <InputLabel shrink={true} htmlFor={id}>
-        {label || schema.title}
-      </InputLabel>
-      <Select
-        multiple={typeof multiple === 'undefined' ? false : multiple}
-        value={typeof value === 'undefined' ? emptyValue : value}
-        required={required}
-        disabled={disabled || readonly}
-        autoFocus={autofocus}
-        onChange={_onChange}
-        onBlur={_onBlur}
-        onFocus={_onFocus}
-      >
-        {(enumOptions as any).map(({ value, label }: any, i: number) => {
-          const disabled: any =
-            enumDisabled && (enumDisabled as any).indexOf(value) != -1;
-          return (
-            <MenuItem key={i} value={value} disabled={disabled}>
-              {label}
-            </MenuItem>
-          );
-        })}
-      </Select>
-    </FormControl>
+      {(enumOptions as any).map(({ value, label }: any, i: number) => {
+        const disabled: any =
+          enumDisabled && (enumDisabled as any).indexOf(value) != -1;
+        return (
+          <MenuItem key={i} value={value} disabled={disabled}>
+            {label}
+          </MenuItem>
+        );
+      })}
+    </TextField>
   );
 };
 
