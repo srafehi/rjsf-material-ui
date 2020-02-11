5fe566b39605444becec41352104dcc8222f6897
Shady Rafehi <shadyrafehi@gmail.com>
2020-02-11 20:54:03 +1100

    IconButton - wrap button with Tooltip, change "remove" icon

--- src/IconButton/IconButton.tsx
+++ src/IconButton/IconButton.tsx
@@ -1,29 +1,34 @@
 import React from 'react';
 
-import Button from '@material-ui/core/Button';
+import MuiIconButton from '@material-ui/core/IconButton';
 import Add from '@material-ui/icons/Add';
 import ArrowUpward from '@material-ui/icons/ArrowUpward';
 import ArrowDownward from '@material-ui/icons/ArrowDownward';
-import Remove from '@material-ui/icons/Remove';
+import DeleteOutlineOutlinedIcon from '@material-ui/icons/DeleteOutlineOutlined';
 import { IconButtonProps as MuiIconButtonProps } from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
 
-const mappings: any = {
-  remove: <Remove />,
-  plus: <Add />,
-  'arrow-up': <ArrowUpward />,
-  'arrow-down': <ArrowDownward />,
+const mappings: { [key: string]: any } = {
+  remove: DeleteOutlineOutlinedIcon,
+  plus: Add,
+  'arrow-up': ArrowUpward,
+  'arrow-down': ArrowDownward,
 };
 
 type IconButtonProps = MuiIconButtonProps & {
   icon: string;
+  tooltip?: string;
 };
 
 const IconButton = (props: IconButtonProps) => {
   const { icon, className, ...otherProps } = props;
+  const Icon = mappings[icon];
   return (
-    <Button {...otherProps} size="small">
-      {mappings[icon]}
-    </Button>
+    <Tooltip title={props.tooltip} enterDelay={500}>
+      <MuiIconButton {...otherProps}>
+        <Icon />
+      </MuiIconButton>
+    </Tooltip>
   );
 };
 
