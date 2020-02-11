2a52ddc44017befb5946aab300c96820aa34dd23
Shady Rafehi <shadyrafehi@gmail.com>
2020-02-11 20:50:41 +1100

    ErrorList - Use "@material-ui/lab/Alert" with severity of "error"

--- src/ErrorList/ErrorList.tsx
+++ src/ErrorList/ErrorList.tsx
@@ -1,36 +1,29 @@
 import React from 'react';
 
 import Box from '@material-ui/core/Box';
-import Typography from '@material-ui/core/Typography';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ErrorIcon from '@material-ui/icons/Error';
 import ListItemText from '@material-ui/core/ListItemText';
-import Paper from '@material-ui/core/Paper';
 
 import { ErrorListProps } from 'react-jsonschema-form';
+import Alert from '@material-ui/lab/Alert';
+import AlertTitle from '@material-ui/lab/AlertTitle';
 
 const ErrorList = ({ errors }: ErrorListProps) => (
-  <Paper elevation={2}>
-    <Box mb={2} p={2}>
-      <Typography variant="h6" component="h6">
-        Errors
-      </Typography>
+  <Alert severity="error">
+    <AlertTitle>Errors</AlertTitle>
+    <Box>
       <List dense={true}>
         {errors.map((error, i: number) => {
           return (
             <ListItem key={i}>
-              <ListItemIcon>
-                <ErrorIcon color="error" />
-              </ListItemIcon>
               <ListItemText primary={error.stack} />
             </ListItem>
           );
         })}
       </List>
     </Box>
-  </Paper>
+  </Alert>
 );
 
 export default ErrorList;
